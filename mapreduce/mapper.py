1112933a7e3a086b76e1a47c91c96143185bec79
joszamama <joszamama@gmail.com>
2023-03-25 22:49:54 +0100

    feat: add new reducer script

--- mapreduce/mapper.py
+++ mapreduce/mapper.py
@@ -3,16 +3,16 @@
 import sys
 import csv
 
-# Input format: Province/State,Country/Region,Lat,Long,Date,Confirmed,Deaths,Recovered,Active,WHO Region
-# Output format: key-value pairs where the key is the country/region and the date, and the value is an array containing the confirmed cases, deaths, and recovered cases
+# Input format: Province/State,Country/Region,Lat,Long,Date,Confirmed,Deaths,Recovered,Active,WHO Region                                                                                                                                                                                    
+# Output format: key-value pairs where the key is the country/region and the date, and the value is an array containing the confirmed cases, deaths, and recovered cases                                                                                                                    
 
+# Parse each line of the CSV file and emit key-value pairs
 reader = csv.reader(sys.stdin)
-next(reader)  # Skip header row
+next(reader) # Skip header row
 for row in reader:
-    country_region = row[1]
-    date = row[4]
-    confirmed = int(row[5])
-    deaths = int(row[6])
-    recovered = int(row[7])
-    value = [confirmed, deaths, recovered]
-    print('%s;%s;%s' % (country_region, date, value))
+        country_region = row[1]
+        confirmed = int(row[5])
+        deaths = int(row[6])
+        recovered = int(row[7])
+        value = [confirmed, deaths, recovered]
+        print('%s;%s' % (country_region, value))
